70c6b3af7bb62b905712636d47067cf794cf7343
musheabdulhakim <musheabdulhakim99@gmail.com>
2021-12-10 22:07:43 +0000

    contact notification

---
+++ app/Notifications/ContactMessage.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ContactMessage extends Notification
+{
+    use Queueable;
+
+    protected $data;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['database','broadcast','mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                ->subject('New Contact message from website')
+                ->line("You have a new contact message from :".$this->data->name)
+                ->line('The Subject of the message is :'. $this->data->subject)
+                ->line('Below is the main contact message written by '.$this->data->name)
+                ->line($this->data->message)
+                ->action('View Message', route('contacts.show',$this->data->id))
+                ->line('Thank you!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            'name' => $this->data->name,
+            'email' => $this->data->email,
+            'subject' => $this->data->subject,
+            'message' => $this->data->message,
+        ];
+    }
+}
