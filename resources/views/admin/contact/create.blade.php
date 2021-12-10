048f96501a448d9fb961b2b7e6b16012b6d495af
musheabdulhakim <musheabdulhakim99@gmail.com>
2021-12-10 19:07:09 +0000

    admin contact view

---
+++ resources/views/admin/contact/create.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@push('page-css')
+
+@endpush
+
+@section('content')
+<div class="col-12">
+    <div class="card">
+        <div class="card-body">
+            <h4 class="card-title">Create Contact</h4>
+
+            {!! form($form) !!}
+        </div>
+    </div>
+</div>
+@endsection
+
+@push('page-js')
+    
+@endpush
---
+++ resources/views/admin/contact/index.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.app')
+
+<x-assets.datatables />
+
+@section('breadcrumb')
+@can('create-contact')
+<x-buttons.primary :text="'create contact'" :link="route('contacts.create')"  />
+@endcan
+@endsection
+
+@section('content')
+    <div class="col-12">
+        <div class="card">
+            <div class="card-body">
+
+                <h4 class="card-title">Contact List</h4>
+
+                <table id="datatable" class="table table-bordered dt-responsive nowrap"
+                    style="border-collapse: collapse; border-spacing: 0; width: 100%;">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Subject</th>
+                            <th>Message</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        
+                    </tbody>
+                </table>
+
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('page-js')
+    <script>
+        $(document).ready(function(){
+            var table = $('#datatable').DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: "{{route('contacts.index')}}",
+                columns: [
+                    {data: 'name', name: 'name'},
+                    {data: 'email', name: 'email'},
+                    {data: 'subject', name: 'subject'},
+                    {data: 'message', name: 'message', orderable: false, searchable: false},
+                    {data: 'action', name: 'action', orderable: false, searchable: false},
+                ]
+            });
+        })
+    </script>
+@endpush
