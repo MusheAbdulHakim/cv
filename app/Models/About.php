1df18fd98b684cc91f9746068a9d412715583126
musheabdulhakim <musheabdulhakim99@gmail.com>
2021-12-11 21:23:59 +0000

    about model

---
+++ app/Models/About.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class About extends Model
+{
+    use HasFactory;
+
+    protected $table= 'about';
+
+    protected $fillable = [
+        'intro','name','age','residence','address',
+        'email','phone','happy_clients','working_hours',
+        'awards','cv','social_links'
+    ];
+
+    protected $casts = [
+        'social_links'=> 'array'
+    ];
+}
