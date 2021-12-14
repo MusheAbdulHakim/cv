0f6e5ceb6d0d19883cad9d20b13a9f03f0bc99ac
musheabdulhakim <musheabdulhakim99@gmail.com>
2021-12-14 16:27:07 +0000

    resume table

---
+++ database/migrations/2021_12_13_233219_create_resume_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateResumeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('resume', function (Blueprint $table) {
+            $table->id();
+            $table->jsonb('education')->nullable();
+            $table->jsonb('experience')->nullable();
+            $table->jsonb('skills')->nullable();
+            $table->string('knowledges')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('resume');
+    }
+}
