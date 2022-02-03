f82fa5cb66d889efa836776611f906959deb82d2
Musheabdulhakim <musheabdulhakim99@gmail.com>
2022-02-03 14:28:17 +0000

    demo seeder added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,10 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
         $this->call([
             RolePermissionSeeder::class,
             UserSeeder::class,
         ]);
+        \App\Models\User::factory(10)->create();
+        \App\Models\Post::factory(10)->create();
+        \App\Models\Client::factory(10)->create();
+        \App\Models\About::factory(1)->create();
+
     }
 }
